9bb389f0eece3fe6e42576d7c1f943c856d9af50
Eric DiMarzio <127116933+EricDiMarzio@users.noreply.github.com>
2023-08-14 14:11:15 -0500

    Add files via upload

--- 003-flag-guessr/script.js
+++ 003-flag-guessr/script.js
@@ -15,18 +15,25 @@ const lives = document.querySelector('.lives');
 const answers = [answerA, answerB, answerC, answerD];
 
 let countryList = new Map();
+let correctCountries = new Map();
+let randomCountryNumber;
+let wrongAnswer;
 let activeCountry;
 let randomNumber;
-let keyCounter = 1;
+let keyCounter = 0;
 let activeCountryName;
 
 const newGame = function () {
   flagsGrid.innerHTML = '';
   countryList.clear();
+  correctCountries.clear();
   keyCounter = 0;
   document.querySelector('.lives').innerHTML = '❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️';
   const request = fetch(`https://restcountries.com/v3.1/all`)
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) throw new Error(`Error message: ${response.status}`);
+      return response.json();
+    })
     .then((data) => {
       data.forEach((entry) => {
         if (entry.flags.png && entry.name.common) {
@@ -49,17 +56,32 @@ const newGame = function () {
     });
 };
 
+const getRandomCountryNumber = function () {
+  while (true) {
+    randomCountryNumber = Math.floor(Math.random() * 250);
+    if (!correctCountries.has(randomCountryNumber)) {
+      return randomCountryNumber;
+    }
+  }
+};
+
+const getWrongAnswer = function () {
+  while (true) {
+    wrongAnswer = Math.floor(Math.random() * 250);
+    if (wrongAnswer !== randomCountryNumber) {
+      return wrongAnswer;
+    }
+  }
+};
+
 const renderNewCountry = function () {
   if (lives.textContent === '') {
     document.querySelector('.modal').showModal();
     return;
   }
-  randomNumber = Math.floor(Math.random() * countryList.size);
-  // console.log(`Step 1: Random Number is : ${randomNumber}`);
-  activeCountry = countryList.get(randomNumber);
+  activeCountry = countryList.get(getRandomCountryNumber());
   activeCountryName = activeCountry.commonName;
 
-  // console.log(activeCountry);
   let randomArray = [];
   for (let i = 0; i < 30; i++) {
     let answerNumber = Math.floor(Math.random() * 4);
@@ -73,22 +95,9 @@ const renderNewCountry = function () {
   }, []);
   console.log(activeCountryName);
 
-  // let r = Math.floor(Math.random() * countryList.size);
-  // const wrongAnswer1 = countryList.get(r).commonName;
-  // r = Math.floor(Math.random() * countryList.size);
-  // const wrongAnswer2 = countryList.get(r).commonName;
-  // r = Math.floor(Math.random() * countryList.size);
-  // const wrongAnswer3 = countryList.get(r).commonName;
-
-  let r = Math.floor(Math.random() * countryList.size);
-  let wrongCountry = countryList.get(r);
-  const wrongAnswer1 = wrongCountry.commonName;
-  r = Math.floor(Math.random() * countryList.size);
-  wrongCountry = countryList.get(r);
-  const wrongAnswer2 = wrongCountry.commonName;
-  r = Math.floor(Math.random() * countryList.size);
-  wrongCountry = countryList.get(r);
-  const wrongAnswer3 = wrongCountry.commonName;
+  const wrongAnswer1 = countryList.get(getWrongAnswer()).commonName;
+  const wrongAnswer2 = countryList.get(getWrongAnswer()).commonName;
+  const wrongAnswer3 = countryList.get(getWrongAnswer()).commonName;
 
   answers[filteredArray[0]].textContent = activeCountry.commonName;
   answers[filteredArray[1]].textContent = wrongAnswer1;
@@ -98,31 +107,7 @@ const renderNewCountry = function () {
   activeFlag.src = activeCountry.flagPNG;
 };
 
-const updateAnswers = function () {
-  let randomArray = [];
-  for (let i = 0; i < 30; i++) {
-    let randomNumber = Math.floor(Math.random() * 4);
-    randomArray.push(randomNumber);
-  }
-  const filteredArray = randomArray.reduce((accumulator, i) => {
-    if (!accumulator.includes(i)) {
-      accumulator.push(i);
-    }
-    return accumulator;
-  }, []);
-
-  let r = Math.floor(Math.random() * countryList.size);
-  const wrongAnswer1 = countryList.get(r).commonName;
-  r = Math.floor(Math.random() * countryList.size);
-  const wrongAnswer2 = countryList.get(r).commonName;
-  r = Math.floor(Math.random() * countryList.size);
-  const wrongAnswer3 = countryList.get(r).commonName;
-
-  answers[filteredArray[0]].textContent = activeCountry.commonName;
-  answers[filteredArray[1]].textContent = wrongAnswer1;
-  answers[filteredArray[2]].textContent = wrongAnswer2;
-  answers[filteredArray[3]].textContent = wrongAnswer3;
-};
+const updateAnswers = function () {};
 
 const addFlagToGrid = function () {
   const newFlag = document.createElement('div');
@@ -131,8 +116,8 @@ const addFlagToGrid = function () {
   flagsGrid.appendChild(newFlag);
 };
 
-const removeCountry = function () {
-  countryList.delete(randomNumber);
+const addToCorrect = function () {
+  correctCountries.set(randomCountryNumber, activeCountry);
 };
 
 const loseALife = function () {
@@ -148,8 +133,8 @@ answersContainer.addEventListener('click', function (e) {
     setTimeout(function () {
       e.target.classList.remove('correct');
       addFlagToGrid();
-      // removeCountry();
-      // countryList.delete(randomNumber);
+      addToCorrect();
+
       renderNewCountry();
     }, 300);
   }
